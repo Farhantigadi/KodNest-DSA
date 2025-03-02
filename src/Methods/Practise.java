b948fe811761cf8b2470b6e226d1deb4e5a07dd7
Farhantigadi <farhantigadi123@gmail.com>
2025-03-02 23:33:54 +0530

    practised method

---
+++ src/Methods/Practise.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class Practise {
+    public static void main(String[] args) {
+        hello("farhan");
+    }
+    static void hello(String name){
+        System.out.println("Hello "+name+"Welcome");
+    }
+}
