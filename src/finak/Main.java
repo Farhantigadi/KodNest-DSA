4e697d4f72c714acd4ba835d5026d739969c9ff2
Farhantigadi <farhantigadi123@gmail.com>
2025-04-16 17:57:07 +0530

    upcasting

---
+++ src/finak/Main.java
@@ -0,0 +1,19 @@
+package finak;
+
+ public class Main {
+    public static void main(String[] args) {
+
+    }
+      void display() {
+        System.out.println("hello");
+    }
+}
+class Main2 extends Main {
+    public static void main(String[] args) {
+
+    }
+    void display(){
+        System.out.println("world");
+    }
+}
+
