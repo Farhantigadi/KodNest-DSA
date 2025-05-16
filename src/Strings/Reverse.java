88a8a8b014f0aba4f4e43f86ad696318c20fd8c8
Farhantigadi <farhantigadi123@gmail.com>
2025-05-16 22:53:11 +0530

    Reverse string new approaches

--- src/Strings/Reverse.java
+++ src/Strings/Reverse.java
@@ -2,19 +2,28 @@ package Strings;
 
 public class Reverse {
     public static void main(String[] args) {
-
+   reverse("hello");
     }
-    public static void reverse(String str){
-        int start=str.charAt(0);
-        int end = str.length()-1;
-        while (start < end){
+    public static void reverse(String  str){
+        char []str2 = str.toCharArray();
+        int start=0;
+        int end = str2.length-1;
 
+        while (start < end){
+     char temp = str2[start];
+            str2[start]=str2[end];
+            str2[end]=temp;
+            start++;
+            end--;
         }
+        System.out.println(str2);
     }
 }
 class reverse2 {
     public static void main(String[] args) {
-        String str2 = "Hello";
+        String str2 = "Hello World";
+        //to remove spaces
+       //   str2=str2.replaceAll("\\s","");
         String rev ="";
         for (int i = str2.length()-1; i >=0 ; i--) {
            rev += str2.charAt(i);
